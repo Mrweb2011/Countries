89a2dad84f53531c5d5bdf8725a127093bf77c96
Mukhammadrasul2011 <msamaritdinov@gmail.com>
2024-10-04 12:02:38 +0500

    Countries

---
+++ app.js
@@ -0,0 +1,14 @@
+let black = document.getElementById("black");
+
+let white = document.getElementById("white");
+
+black.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#202C36"
+})
+white.addEventListener("click", () => {
+    document.body.style.backgroundColor = "White"
+})
+
+
+
+
